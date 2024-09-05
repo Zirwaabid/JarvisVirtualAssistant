43f105c80dc6f99e6f39c02cd5b09f18c881832c
Zirwa Abid <zirwaabid112@gmail.com>
2024-09-05 16:05:11 -0700

    project

---
+++ Jarvis.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector('.talk');
+const content = document.querySelector('.content');
+
+function speak(text) {
+    const text_speak = new SpeechSynthesisUtterance(text);
+
+    text_speak.rate = 1;
+    text_speak.volume = 1;
+    text_speak.pitch = 1;
+
+    window.speechSynthesis.speak(text_speak);
+}
+
+function wishMe() {}
